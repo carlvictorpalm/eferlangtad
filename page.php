4cd801b56f4eb6b7460b75e617bd609c874fda28
Victor Palm <carlvictorpalm@gmail.com>
2018-01-17 19:00:03 +0100

    updated

--- page.php
+++ page.php
@@ -12,9 +12,12 @@
                         <input type="text" id="birthnameText" placeholder="FÖDELSENAMN" name="birthname">
                         <input type="text" id="birthlength" placeholder="FÖDELSELÄNGD" name="birthlength">
                         <input type="date" id="birthday" placeholder="FÖDELSEDAG" name="birthday">
-                        <span>FÖDELSETID</span><input class="time1" type="text" id="birthtime" placeholder="" name="birthtime">
-                        <span>:</span><input class="time2" type="text" id="birthtime" placeholder="" name="birthtime">
                         <input type="text" id="birthweight" placeholder="FÖDELSEVIKT" name="birthweight">
+                        <br>
+                        <div class="birthtimefield">
+                            <span>FÖDELSETID</span><input class="time1" type="text" id="birthtime" placeholder="" name="birthtime">
+                            <span>:</span><input class="time2" type="text" id="birthtime" placeholder="" name="birthtime">
+                        </div>
                     </form>
                     <div class="formbutton">
                         <button class="btn btn-success" type="button">LÄGG TILL I VARUKORGEN</button>
@@ -26,8 +29,8 @@
                 <div class="innerpicture">
                     <img src="<?php echo get_bloginfo('template_url') ?>/img/bild.jpg" alt="">
                     <div class="birthnameoutput"></div>
-                    <div class="birthlengthoutput"></div>
-                    <div class="birthweightoutput"></div>
+                    <div class="birthlengthoutput" maxlength="2"></div>
+                    <div class="birthweightoutput" maxlength="4"></div>
                     <div class="birthdayoutput"></div>
                     <div class="birthtimeoutput"></div>
                 </div>
